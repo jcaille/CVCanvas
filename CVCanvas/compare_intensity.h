c26b00994ff7401613873f96bb3f60b1618a2eb1
NTheo <mrshadow.night@gmail.com>
2014-04-02 11:01:18 +0200

    added the compare functions to compare intensity betwwen files corrected vector iterations for compatibility issues testing git. Wow. Such comments. So C++. Many commits.

---
+++ CVCanvas/compare_intensity.h
@@ -0,0 +1,6 @@
+///@author Jean Caille, Theophile Dalens, 2014
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+void compare(const cv::Mat& imx, const cv::Mat& imy, const std::string& output_file_name);

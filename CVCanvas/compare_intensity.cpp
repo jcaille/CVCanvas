4d427f7dbd3694f0f13aee003b2120601108553a
Jean Caillé <jcaille@gmail.com>
2014-04-02 14:00:06 +0200

    Fixed some stuff for my OCD

--- CVCanvas/compare_intensity.cpp
+++ CVCanvas/compare_intensity.cpp
@@ -1,17 +1,18 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <fstream>
 #include "compare_intensity.h"
-using namespace std;
-using namespace cv;
-void compare(const Mat& imx, const Mat& imy, const string& output_file_name)
-{
-	ofstream compare;
-	compare.open(output_file_name);
-	for(int i=0; i<imx.rows; i++)
-		for(int j=0; j<imy.cols; j++)
-			compare << i << " " << j << " " << (int)imx.at<uchar>(i,j) << " " << (int)imy.at<uchar>(i,j) << std::endl;
-	compare.close();
 
+using namespace cv;
+
+void compare(const Mat& imx, const Mat& imy, const string& outputFilePath)
+{
+    std::ofstream comparisonFile;
+    comparisonFile.open(outputFilePath);
+	
+    for(int i=0; i<imx.rows; i++) {
+		for(int j=0; j<imy.cols; j++) {
+			comparisonFile << i << " " << j << " " << (int)imx.at<uchar>(i,j) << " " << (int)imy.at<uchar>(i,j) << std::endl;
+        }
+    }
+    
+	comparisonFile.close();
 }
